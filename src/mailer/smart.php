a0241af9cfc63c61a29c0c80b10769802b67037e
Daniel <98545742+NeYRoX121@users.noreply.github.com>
2022-08-15 22:52:56 +0300

    Update smart.php

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'dan_121997@mail.ru';                 // Наш логин
-$mail->Password = 'svBJwyUs6je9tnX0GRiP';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
